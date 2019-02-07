68edae50e7203512ee46ea6d6c7f0f09f5c1ec2f
Derek Houck <derek@derekhouck.com>
2019-02-07 09:06:02 -0800

    Add validators to add game form

--- src/components/game-form.js
+++ src/components/game-form.js
@@ -1,15 +1,57 @@
 import React from 'react';
+import { reduxForm, Field, focus } from 'redux-form';
+import Input from './input';
+import { required, nonEmpty, minNum, maxNum, notLessThanField } from '../validators';
+
+const playersMin = minNum(1);
+const playersMax = maxNum(99);
+const notLessThanMinPlayers = notLessThanField('minPlayers');
 
 export class GameForm extends React.Component {
-  render () {
+  onSubmit (values) {
+    const { title, minPlayers, maxPlayers } = values;
+    const game = { title, minPlayers, maxPlayers };
+    console.log(game);
+  }
+
+  render() {
     return (
       <form
         className="game-form"
+        onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
       >
         <h1>Add a Game</h1>
+        <Field 
+          component={Input}
+          type="text"
+          name="title"
+          id="title"
+          label="Game title"
+          validate={[required, nonEmpty]}
+        />
+        <Field 
+          component={Input}
+          type="number"
+          name="minPlayers"
+          id="minPlayers"
+          label="Minimum number of players"
+          validate={[ playersMin, playersMax ]}
+        />
+        <Field 
+          component={Input}
+          type="number"
+          name="maxPlayers"
+          id="maxPlayers"
+          label="Maximum number of players"
+          validate={[ playersMin, playersMax, notLessThanMinPlayers ]}
+        />
+        <button type="submit">Add Game</button>
       </form>
     );
   }
 }
 
-export default GameForm;
+export default reduxForm({
+  form: 'game-form',
+  onSubmitFail: (errors, dispatch) => dispatch(focus('registration', Object.keys(errors)[0]))
+})(GameForm);
--- src/validators.js
+++ src/validators.js
@@ -1,8 +1,11 @@
 export const required = value => (value ? undefined : 'Required');
+
 export const nonEmpty = value =>
     value.trim() !== '' ? undefined : 'Cannot be empty';
+
 export const isTrimmed = value =>
     value.trim() === value ? undefined : 'Cannot start or end with whitespace';
+
 export const length = length => value => {
     if (length.min && value.length < length.min) {
         return `Must be at least ${length.min} characters long`;
@@ -11,7 +14,19 @@ export const length = length => value => {
         return `Must be at most ${length.max} characters long`;
     }
 };
+
 export const matches = field => (value, allValues) =>
     field in allValues && value.trim() === allValues[field].trim()
         ? undefined
         : 'Does not match';
+
+export const minNum = num => value =>
+    value >= num ? undefined : `Must be at least ${num}`;
+
+export const maxNum = num => value =>
+    value <= num ? undefined : `Must be at most ${num}`;
+
+export const notLessThanField = field => (value, allValues) =>
+    field in allValues && Number(value) >= Number(allValues[field])
+        ? undefined
+        : `Must not be less than ${allValues[field]}`;
