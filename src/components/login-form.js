5c08a6f5907579fd8e89cb1c8c5a8fbf9a780647
Derek Houck <derek@derekhouck.com>
2019-02-07 13:55:04 -0800

    Add logging in message

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
+import { Field, reduxForm, focus } from 'redux-form';
+import { connect } from 'react-redux';
 import Input from './input';
-import {login} from '../actions/auth';
-import {required, nonEmpty} from '../validators';
+import { login } from '../actions/auth';
+import { required, nonEmpty } from '../validators';
 
 export class LoginForm extends React.Component {
     onSubmit(values) {
@@ -10,7 +11,7 @@ export class LoginForm extends React.Component {
     }
 
     render() {
-        let error;
+        let error, body;
         if (this.props.error) {
             error = (
                 <div className="form-error" aria-live="polite">
@@ -18,38 +19,52 @@ export class LoginForm extends React.Component {
                 </div>
             );
         }
-        return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                {error}
-                <label htmlFor="username">Username</label>
-                <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    id="username"
-                    validate={[required, nonEmpty]}
-                />
-                <label htmlFor="password">Password</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    validate={[required, nonEmpty]}
-                />
-                <button disabled={this.props.pristine || this.props.submitting}>
-                    Log in
+        if (this.props.loading) {
+            body = (
+                <div className="message message-default">
+                    Logging you in...
+                </div>
+            );
+        } else {
+            body = (
+                <form
+                    className="login-form"
+                    onSubmit={this.props.handleSubmit(values =>
+                        this.onSubmit(values)
+                    )}>
+                    {error}
+                    <label htmlFor="username">Username</label>
+                    <Field
+                        component={Input}
+                        type="text"
+                        name="username"
+                        id="username"
+                        validate={[required, nonEmpty]}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <Field
+                        component={Input}
+                        type="password"
+                        name="password"
+                        id="password"
+                        validate={[required, nonEmpty]}
+                    />
+                    <button disabled={this.props.pristine || this.props.submitting}>
+                        Log in
                 </button>
-            </form>
-        );
+                </form>
+            );
+        }
+        return body;
     }
 }
+
+const mapStateToProps = state => ({
+    error: state.auth.error,
+    loading: state.auth.loading
+});
 
 export default reduxForm({
     form: 'login',
     onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
-})(LoginForm);
+})(connect(mapStateToProps)(LoginForm));
