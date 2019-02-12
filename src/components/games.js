c12a639793801b381d4cf7684c1e1aefa8cd688a
Derek Houck <derek@derekhouck.com>
2019-02-12 09:10:13 -0800

    Add message for empty game list

--- src/components/games.js
+++ src/components/games.js
@@ -24,32 +24,44 @@ export class Games extends React.Component {
         <div className="message message-default">Loading users...</div>
       );
     } else {
-      const games = this.props.games.map(game => {
-        const tags = game.tags.map(tag => (<li key={tag.id} id={tag.id}>{tag.name}</li>));
-        return (
-          <li className="game" key={game.id} id={game.id}>
-            <h3 className="game__title">{game.title}</h3>
-            <ul className="game__details">
-              <li><strong>Players:</strong> {game.players.min} - {game.players.max}</li>
-              <li>
-                <strong>Tags</strong>
-                <ul className="game__tag-list">
-                  {tags}
-                </ul>
-              </li>
-            </ul>
-            <section className="game__buttons">
-              <Link to={`/games/${game.id}/edit`}>
-                <Button label="Edit" />
-              </Link>
-              <Button 
-                onClick={() => this.props.dispatch(deleteGame(game))}
-                label="Remove"
-              />
-            </section>
-          </li>
+      let games;
+      if (this.props.games.length === 0) {
+        games = (
+          <div className="games__no-games">
+            <p>You don't have any games at the moment. Let's add one now.</p>
+            <Link to="/games/add">
+              <Button primary label="Add a game" />
+            </Link>
+          </div>
         );
-      });
+      } else {
+        games = this.props.games.map(game => {
+          const tags = game.tags.map(tag => (<li key={tag.id} id={tag.id}>{tag.name}</li>));
+          return (
+            <li className="game" key={game.id} id={game.id}>
+              <h3 className="game__title">{game.title}</h3>
+              <ul className="game__details">
+                <li><strong>Players:</strong> {game.players.min} - {game.players.max}</li>
+                <li>
+                  <strong>Tags</strong>
+                  <ul className="game__tag-list">
+                    {tags}
+                  </ul>
+                </li>
+              </ul>
+              <section className="game__buttons">
+                <Link to={`/games/${game.id}/edit`}>
+                  <Button label="Edit" />
+                </Link>
+                <Button
+                  onClick={() => this.props.dispatch(deleteGame(game))}
+                  label="Remove"
+                />
+              </section>
+            </li>
+          );
+        });
+      }
       body = (
         <section className="games">
           <h2>Your Games</h2>
@@ -61,10 +73,10 @@ export class Games extends React.Component {
     }
     return body;
   }
-  
+
   render() {
     return (
-      <section>
+      <section className="games__wrapper">
         <header className="games-header">
           <Link to="/games/add" className="centered"><Button primary label="Add a game" /></Link>
           <GamesSearchFrom />
