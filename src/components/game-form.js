7766982f9a3d0280e3916042aa1b8e54393c01b7
Derek Houck <derek@derekhouck.com>
2019-02-15 08:42:42 -0800

    Fix first option issue with select multiple element

--- src/components/game-form.js
+++ src/components/game-form.js
@@ -17,7 +17,7 @@ const multiSelectOptionMarkup = (text) => (
   <div>
     <span className="checkbox">
       <svg className="checkbox-icon" x="0px" y="0px" width="12px" height="12px" viewBox="0 0 488.878 488.878">
-        <g><polygon points="143.294,340.058 50.837,247.602 0,298.439 122.009,420.447 122.149,420.306 144.423,442.58 488.878,98.123 437.055,46.298 "/></g>
+        <g><polygon points="143.294,340.058 50.837,247.602 0,298.439 122.009,420.447 122.149,420.306 144.423,442.58 488.878,98.123 437.055,46.298 " /></g>
       </svg>
     </span>
     <span> {text}</span>
@@ -67,9 +67,11 @@ export class GameForm extends React.Component {
   }
 
   render() {
-    const tags = this.props.tags.sort((a,b) => a.name.localeCompare(b.name)).map(tag => ({
+    const sortedTags = this.props.tags.sort((a, b) => a.name.localeCompare(b.name));
+    const tagOptions = [{ id: null, name: 'No tags' }, ...sortedTags];
+    const tags = tagOptions.map(tag => ({
       markup: multiSelectOptionMarkup(tag.name),
-      value: tag.id, 
+      value: tag.id,
       text: tag.name
     }));
 
--- src/components/game-form.test.js
+++ src/components/game-form.test.js
@@ -3,9 +3,25 @@ import { shallow } from 'enzyme';
 import { GameForm } from './game-form';
 
 describe('<GameForm />', function () {
+  const mockFetchTagAction = {
+    type: 'FETCH_TAGS'
+  };
+  jest.mock('../actions/games', () => Object.assign({}, require.requireActual('../actions/games'), {
+    fetchTags: jest.fn().mockImplementation(() => {
+      return mockFetchTagAction;
+    })
+  }));
+
   it('renders without crashing', () => {
     const callback = jest.fn();
     const promise = jest.fn().mockImplementation(() => Promise.resolve());
     shallow(<GameForm tags={[]} handleSubmit={callback} dispatch={promise} />);
+  });
+
+  it('should call dispatch when mounted', function () {
+    const handleSubmit = jest.fn();
+    const dispatch = jest.fn(() => Promise.resolve());
+    shallow(<GameForm tags={[]} handleSubmit={handleSubmit} dispatch={dispatch} />);
+    expect(dispatch).toHaveBeenCalled();
   });
 });
