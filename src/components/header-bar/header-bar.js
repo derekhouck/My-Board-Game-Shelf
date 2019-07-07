2bd26b99653fbb5f2ad8fc31fabb0924f88e5bc9
Derek Houck <derek@derekhouck.com>
2019-07-07 07:08:51 -0700

    Add admin menu to header nav

--- src/components/header-bar/header-bar.js
+++ src/components/header-bar/header-bar.js
@@ -5,6 +5,7 @@ import { clearAuth } from '../../actions/auth';
 import { clearAuthToken } from '../../local-storage';
 import { action as toggleMenu } from 'redux-burger-menu';
 import logo from './logo.svg';
+import './header-bar.css';
 
 import Menu from './Menu';
 import MenuButton from './menu-button';
@@ -17,15 +18,21 @@ export class HeaderBar extends React.Component {
   }
 
   render() {
-    // Only render the log out button if we are logged in
+    const { currentUser, dispatch, loggedIn } = this.props;
     let headerNav;
+    const adminMenu = (
+      <div className="admin-menu">
+        <Link to="/users" onClick={() => dispatch(toggleMenu(false))}>Users</Link>
+      </div>
+    );
     const appTitle = (
-      <Link to={this.props.loggedIn ? '/dashboard' : '/'} className="App-title">
+      <Link to={loggedIn ? '/dashboard' : '/'} className="App-title">
         <img src={logo} alt="My Board Game Shelf" />
       </Link>
     );
 
-    if (this.props.loggedIn) {
+    // Only render the log out button if we are logged in
+    if (loggedIn) {
       headerNav = (
         <Menu
           right
@@ -35,12 +42,13 @@ export class HeaderBar extends React.Component {
         >
           <Link
             to="/games/add"
-            onClick={() => this.props.dispatch(toggleMenu(false))}
+            onClick={() => dispatch(toggleMenu(false))}
           >
             Add a game
           </Link>
-          <Link to="/account" onClick={() => this.props.dispatch(toggleMenu(false))}>Your account</Link>
+          <Link to="/account" onClick={() => dispatch(toggleMenu(false))}>Your account</Link>
           <button className="App-header__logout" onClick={() => this.logOut()}>Log out</button>
+          {currentUser && currentUser.admin ? adminMenu : null}
         </Menu>
       );
     } else {
@@ -57,6 +65,7 @@ export class HeaderBar extends React.Component {
 }
 
 const mapStateToProps = state => ({
+  currentUser: state.auth.currentUser,
   loggedIn: state.auth.currentUser !== null
 });
 
