30b9c4f5daab643e6e351546b39cb04e86d7161f
Derek Houck <derek@derekhouck.com>
2019-11-02 17:36:05 -0700

    Style GamePage

--- src/components/pages/game-page.js
+++ src/components/pages/game-page.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Button from '../button';
+import './game-page.css';
 
 export const GamePage = (props) => {
-  return (<div>GamePage</div>);
+  return (
+    <article className="game-page">
+      <header className="game-page__header">
+        <h1>GamePage</h1>
+        <Button
+          className="btn--add game-page__btn"
+          label="Add"
+        />
+      </header>
+      <section className="game-page__details">
+        <h2>Players</h2>
+        <div>2-8</div>
+        <h2>Themes</h2>
+        <ul>
+          <li>Tag 1</li>
+          <li>Tag 2</li>
+        </ul>
+        <h2>Mechanics</h2>
+        <ul>
+          <li>Tag 1</li>
+          <li>Tag 2</li>
+        </ul>
+      </section>
+    </article>
+  );
 };
 
 export default GamePage;
